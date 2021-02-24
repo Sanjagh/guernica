8790746fcbf690f2d3c2000d045084aa26ad2900
Ahmad <Eahmad.edge@gmail.com>
2021-02-24 06:55:36 +0330

    Finalize request model

--- engine/src/main/scala/pro/sanjagh/guernica/application/model/NewJobRequest.scala
+++ engine/src/main/scala/pro/sanjagh/guernica/application/model/NewJobRequest.scala
@@ -1,14 +1,15 @@
 package pro.sanjagh.guernica.application.model
 
-final case class Degree private(value: Double) extends AnyVal
-final case class Rect private(width: Double, height: Double)
-
 sealed trait Action
 object Action {
+  final case class Degree private (value: Double) extends AnyVal
+  final case class Rect private (width: Double, height: Double)
+
   case object Optimize extends Action
   final case class Rotate(degree: Degree) extends Action
   final case class Crop(rect: Rect) extends Action
   final case class Resize(rect: Rect) extends Action
+  final case class CropAndResize(cropRect: Rect, resizeRect: Rect) extends Action
 }
 
-class NewJobRequest(actions: Seq[Action], inputBucket: String, outputBucket: String)
+class NewJobRequest(actions: Action, inputBucket: String, outputBucket: String)
